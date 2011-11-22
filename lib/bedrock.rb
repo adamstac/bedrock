842162a26c30893e127c3f3f4d15bbe0789eb5ac
Adam Stacoviak <adam@stacoviak.com>
2011-11-21 23:27:20 -0600

    Bumped the version and added a changelog

--- lib/bedrock.rb
+++ lib/bedrock.rb
@@ -3,7 +3,7 @@ Compass::Frameworks.register("bedrock", :path => "#{File.dirname(__FILE__)}/..")
 
 module Bedrock
   
-  VERSION = "0.0.1"
-  DATE = "2011-10-04"
+  VERSION = "0.0.2"
+  DATE = "2011-11-21"
 
 end
